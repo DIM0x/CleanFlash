2ebc6b9a41a3ba25b94a43785dda201da60cb378
darktohka <daniel@tohka.us>
2023-09-04 19:27:04 +0300

    cleanflash: Update to 34.0.0.301

--- CleanFlashCommon/UpdateChecker.cs
+++ CleanFlashCommon/UpdateChecker.cs
@@ -32,8 +32,8 @@ namespace CleanFlashCommon {
     }
 
     public class UpdateChecker {
-        private static readonly string FLASH_VERSION = "34.0.0.295";
-        private static readonly string VERSION = "34.0.0.295";
+        private static readonly string FLASH_VERSION = "34.0.0.301";
+        private static readonly string VERSION = "34.0.0.301";
         private static readonly string AUTHOR = "cleanflash";
         private static readonly string REPO = "installer";
         private static readonly string USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36";
